8adc464a77860fbc6163c1bb67805529620c097c
AceMelody <omotranyango@gmail.com>
2023-09-25 05:11:44 -0700

    Arrays and pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the sring pointed to by @src to the buffer pointed to by @ dest
+ * @*src: points to the source string
+ * @*dest: points to where the string is copied
+ *
+ * Return: the string that is copied
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	
+}
