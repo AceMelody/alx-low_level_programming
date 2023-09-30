80ef20c626cdb55a9ee84b000dba915cabacd471
AceMelody <omotranyango@gmail.com>
2023-09-30 08:29:58 -0700

    Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,11 +16,11 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (((b > a && a > c) || (b > c && c > a)) || ( b == c && c > a))
+	else if (((b > a && a > c) || (b > c && c > a)) || (b == c && c > a))
 	{
-	        largest = b;
+		largest = b;
 	}
-	else 
+	else
 	{
 		largest = c;
 	}
