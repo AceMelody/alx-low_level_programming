18ddf4d613c3807c9befd4bd20108d26c17e12f4
AceMelody <omotranyango@gmail.com>
2023-09-30 04:10:41 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,9 +2,9 @@
 
 /**
  * print_sign - prints the sign that precedes an integer
- * @n : the number whose sign is checked
+ * @n: the number whose sign is checked
  *
- * Return : 1, if posistive, 0, if zero, -1, if negative
+ * Return: 1, if posistive, 0, if zero, -1, if negative
  */
 
 int print_sign(int n)
