332d3309c3e6d3d917bdc6ec6c58c3679e94962a
AceMelody <omotranyango@gmail.com>
2023-10-03 04:50:28 -0700

    Adjustment5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,30 +2,13 @@
 
 /**
  * rev_string - reverses a string
- * @s: points to the string that is to be reversed
- *
+ * @s: points to the string
  *
  * Return: nothing
  */
 
 void rev_string(char *s)
 {
-	int f, v;
-	char *k[] = s;
-
-	v = _strlen(s) - 1;
-	for (f = v; f >= 0; f--)
-		_ putchar(k[f]);
-}
-
-int _strlen(char *s)
-{
-	int n = 0;
-
-	while (*s != '\0')
-	{
-		n++;
-		s++;
-	}
-	return (n);
+	for (*s = '\0'; s--; --s)
+		_putchar(*s);
 }
