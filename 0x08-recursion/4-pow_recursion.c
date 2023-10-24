a60d16ebe06e918bf9158173c9541687956f1fc9
AceMelody <omotranyango@gmail.com>
2023-10-24 10:01:33 -0700

    0x08-recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -18,9 +18,12 @@ int _pow_recursion(int x, int y)
 	{
 		return (1);
 	}
+	else if (y == 1)
+	{
+		return(x);
+	}
 	else
 	{
-		x *= x;
-		return (0 + _pow_recursion(x, y - 1));
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
