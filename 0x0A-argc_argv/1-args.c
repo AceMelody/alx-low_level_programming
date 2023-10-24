470cfa8d04037099e32178b0f98c11e68fd5e0de
AceMelody <omotranyango@gmail.com>
2023-10-24 09:25:53 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,13 +3,13 @@
 
 /**
  * main - prints the number of arguments passed into this program
- * @agrc: the number of arguments
+ * @argc: the number of arguments
  * @argv: an array of the arguments
  *
  * Return: 0, on success
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int n;
 
@@ -17,5 +17,5 @@ int main (int argc, char *argv[])
 	(void)argv;
 	_putchar(n + '0');
 	_putchar('\n');
-	return(0);
+	return (0);
 }
