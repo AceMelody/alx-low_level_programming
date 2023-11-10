cef08d63390e75f7a8abb69d5e5d3fc50fcc2549
AceMelody <omotranyango@gmail.com>
2023-11-10 04:20:32 -0800

    Structures and typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes struct dog
+ * @d: the struct name
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the name of the dog's owner
+ *
+ * Return: nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints struct dog
+ * @d: The dog's details
+ *
+ * Return: nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", d->name);
+	printf("Age: %.6f\n", d->age);
+	printf("Owner: %s\n", d->ownee);
+}
