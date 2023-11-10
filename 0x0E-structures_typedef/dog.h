2208e9a12677b83f70b61937eeb9229072c46801
AceMelody <omotranyango@gmail.com>
2023-11-10 04:32:47 -0800

    Structures and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef DOG
 #define DOG
 
+/**
+ * struct dog - has the attributes of a dog
+ * @nane: the dog's name
+ * @age: age of the dog
+ * @owner: name of the person who owns the dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 #endif /* DOG */
