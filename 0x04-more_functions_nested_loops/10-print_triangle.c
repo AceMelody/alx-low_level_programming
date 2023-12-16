bce3bdb75d5c74a324e69cb7034c48b61df099a8
AceMelody <omotranyango@gmail.com>
2023-12-16 06:51:36 -0800

    More functions, nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,5 +8,19 @@
  */
 void print_triangle(int size)
 {
+	int j, k;
 
+	if (size <= 0)
+		_putchar('\n');
+	for (j = 0; j < size; j++)
+	{
+		for (k = 0; k < size; k++)
+		{
+			if (k == (size - (j + 1)))
+				_putchar('#');
+			else
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
 }
