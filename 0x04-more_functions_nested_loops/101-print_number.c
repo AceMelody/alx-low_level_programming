e9bde7e136a19eb8d91a3eb657c80aca7b03deff
AceMelody <omotranyango@gmail.com>
2023-12-20 04:29:12 -0800

    More functions, nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -35,7 +35,7 @@ void print_number(int n)
 			v /= 10;
 		}
 	}
-	else if (k > 9)
+	else if (k >= 9)
 	{
 		while ((k - v) < 0)
 		{
@@ -54,6 +54,6 @@ void print_number(int n)
 			}
 		}
 	}
-	if (n % 10 == 0)
+	if (n % 10 == 0 && n != 0)
 		_putchar(k + '0');
 }
