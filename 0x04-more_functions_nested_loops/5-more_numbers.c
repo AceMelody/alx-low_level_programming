d06d7325fba9db8b4f7d133a34083e5ef0007f01
AceMelody <omotranyango@gmail.com>
2024-01-27 03:58:30 -0800

    More functions, nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,19 +10,15 @@ void more_numbers(void)
 {
 	int v, w;
 
-	for (v = '0'; v <= '9'; v++)
+	for (v = 0; v <= 9; v++)
 	{
 		for (w = 0; w <= 14; w++)
 		{
 			if (w < 9)
 			{
 				_putchar((w / 10) + '0');
-				_putchar((w % 10) + '0');
 			}
-			else
-			{
-				_putchar(w + '0');
-			}
+			_putchar(w % 10 + '0');
 		}
 		_putchar('\n');
 	}
